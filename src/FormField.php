9045cf57eb4e6de0e979c71a8be086271242a48c
Carlos Acuña Campos <caacuna@webmachine.cl>
2017-03-17 19:43:43 -0300

    Agrega inputs password y button, además agrega capacidad de sobreescribir defaults desde config

--- src/FormField.php
+++ src/FormField.php
@@ -3,6 +3,40 @@ namespace Webmachine\FormField;
 
 class FormField {
     
+    /*
+     * What should each form element be
+     * wrapped within?
+    */
+    protected $wrapper;
+
+    /*
+     * What class should the wrapper
+     * element receive?
+    */
+    protected $wrapperClass;
+
+    /**
+     * Should form inputs receive a class name?
+     */
+    protected $inputClass;
+    
+    /**
+     * Should form labels receive a class name?
+     */
+    protected $labelClass;    
+
+    /**
+     * Empty option text for selects
+     */
+    protected $emptyOptionText;      
+    
+    /**
+     * Init config params
+     */
+    public function __construct() {
+        $this->resetConfigs();
+    }
+    
     /**
      * Build Text input
      * 
@@ -14,6 +48,18 @@ class FormField {
         $format = '<input type="text" name="%s" class="%s" id="%s"%s>';
         return $this->format($format, $name, $attributes, 'text');
     }
+    
+    /**
+     * Build Password input
+     * 
+     * @param string $name field name
+     * @param array $attributes
+     * @return string
+     */    
+    public function password($name, $attributes = []) {
+        $format = '<input type="password" name="%s" class="%s" id="%s"%s>';
+        return $this->format($format, $name, $attributes, 'password');
+    }    
     
     /**
      * Build File input
@@ -54,7 +100,7 @@ class FormField {
         $is_assoc = array_keys($options) !== range(0, count($options) - 1);
         
         $label = isset($attributes['label'])? $attributes['label'] : $this->prettifyFieldName($name);
-        $formatted_options = $use_empty_option? [sprintf($option_format, '', config('formfield.emptyOptionText') . ' ' . $label)] : [];
+        $formatted_options = $use_empty_option? [sprintf($option_format, '', $this->emptyOptionText . ' ' . $label)] : [];
         
         foreach ($options as $val => $opt) {
             $val = $is_assoc? $val : $opt;
@@ -64,11 +110,53 @@ class FormField {
         return $this->format($select_format, $name, $attributes, 'textarea');
     }
     
+    /**
+     * Build hidden input
+     * 
+     * @param string $name field name
+     * @param array $attributes
+     * @return string
+     */
     public function hidden($name, $attributes = []) {
         $value = isset($attributes['value'])? $attributes['value'] : '';
-        $id = isset($attributes['id'])? $attributes['id'] : $name;
-        return sprintf('<input type="hidden" name="%s" value="%s" id="%s">', $name, $value, $id);
+        return sprintf('<input type="hidden" name="%s" value="%s" %s>', $name, $value, $this->setAttributes($attributes, ['type', 'name', 'value']));
     }
+    
+    /**
+     * Build button input
+     * 
+     * @param string $label button label
+     * @param array $attributes
+     * @return string
+     */
+    public function button($label, $attributes = []) {
+        $type = isset($attributes['type'])? $attributes['type'] : 'button';
+        return sprintf('<button type="%s" %s>%s</button>', $type,  $this->setAttributes($attributes, ['type']), $label);
+    }    
+    
+    /**
+     * Set Default config
+     * 
+     * @param string $option
+     * @param string $value
+     * @return void
+     */
+    public function setConfig($option, $value) {
+        if(isset($this->$option)) $this->$option = $value;
+    }
+
+    /**
+     * Set properties from config
+     * 
+     * @return void
+     */
+    public function resetConfigs() {
+        $this->wrapper = config('formfield.wrapper');
+        $this->wrapperClass = config('formfield.wrapperClass');
+        $this->inputClass = config('formfield.inputClass');
+        $this->labelClass = config('formfield.labelClass');
+        $this->emptyOptionText = config('formfield.emptyOptionText');
+    }    
     
     /**
      * Field Format
@@ -81,7 +169,7 @@ class FormField {
      */
     protected function format($format, $name, $attributes, $type) {
         $label = $this->createLabel($name, $attributes);
-        $class = isset($attributes['class'])? $attributes['class'] : config('formfield.inputClass');
+        $class = isset($attributes['class'])? $attributes['class'] : $this->inputClass;
         $id = isset($attributes['id'])? $attributes['id'] : $name;
         $field = sprintf($format, $name, $class, $id, $this->setAttributes($attributes));
         return sprintf($this->createWrapper(), $label . "\n" . $field);
@@ -91,12 +179,13 @@ class FormField {
      * Set Atributes from array
      * 
      * @param array $attributes
+     * @param array $banned
      * @return string
      */
-    protected function setAttributes($attributes) {
+    protected function setAttributes($attributes, $banned = ['type', 'class', 'id', 'label']) {
         $result = [];
         foreach ($attributes as $attr => $val) {
-            if(in_array($attr, ['class', 'id'])) continue;
+            if(in_array($attr, $banned)) continue;
             $result[] = "$attr=\"$val\"";
         }
         return implode('', $result);
@@ -108,8 +197,8 @@ class FormField {
      * @return string
      */
     protected function createWrapper() {
-        $wrapper = config('formfield.wrapper');
-        $wrapperClass = config('formfield.wrapperClass');
+        $wrapper = $this->wrapper;
+        $wrapperClass = $this->wrapperClass;
         return "<$wrapper class='$wrapperClass'>%s</$wrapper>";
     }
     
@@ -122,7 +211,7 @@ class FormField {
      */
     protected function createLabel($name, $attributes) {
         $format = '<label class="%s" for="%s">%s</label>';
-        $class = isset($attributes['label_class'])? $attributes['label_class'] : config('formfield.labelClass');
+        $class = isset($attributes['label_class'])? $attributes['label_class'] : $this->labelClass;
         $for = isset($attributes['id'])? $attributes['id'] : $name;
         $label = isset($attributes['label'])? $attributes['label'] : $this->prettifyFieldName($name);
         
@@ -137,6 +226,6 @@ class FormField {
      */
     protected function prettifyFieldName($name) {
         return ucwords(preg_replace('/(?<=\w)(?=[A-Z])/', " $1", $name));
-    }    
+    }
 
 }
